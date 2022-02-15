7284fdb36f5f63af682f337f834037c67593f5e9
G'dion <99226965+dion1717@users.noreply.github.com>
2022-02-15 12:06:00 +0700

    Add files via upload

---
+++ squential/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+    int banyakData, i, cari;
+    int ketemu = 0;
+
+    printf("Pencarian Data dengan Algoritma Sequential Search\n\n");
+    printf("Input Banyak Data\t: ");
+    scanf("%d", &banyakData);
+    int data[banyakData];
+
+    printf("Input Kumpulan Data\t: \n");
+    for(i = 0; i < banyakData; i++)
+    {
+        scanf("%d", &data[i]);
+    }
+
+    printf("Input Data yang Dicari  : ");
+    scanf("%d", &cari);
+
+    for(i = 0; i < banyakData; i++)
+    {
+        if(cari == data[i])
+        {
+            ketemu = 1;
+            printf("Hasil Pencarian \t: Data Ditemukan pada Indeks ke - %d\n", i+1);
+            break;
+        }
+    }
+
+    if(ketemu == 0)
+        printf("Hasil Pencarian \t  : Data Tidak Ditemukan\n");
+
+    getch();
+    return 0;
+}
